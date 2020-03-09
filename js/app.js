80a06adc98bf0d075cc112231d0067b045c24ed5
tjgillweb <tjgillweb@gmail.com>
2020-03-09 20:32:16 +0530

    Add script to add Todo list item

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const submitForm = document.querySelector('.add');
+const addButton = document.querySelector('.add-todo');
+const todoList = document.querySelector('.todos');
+const list = document.querySelectorAll('.todos li'); //gives a NodeList of all li's
+let listLength = list.length;
+
+const generateTemplate = (todo) => {
+
+    const html = `<li>
+                    <input type="checkbox" id="todo_${listLength}" />
+                    <label for="todo_${listLength}">
+                        <span class="check"></span>${todo}
+                    </label>
+                    <i class="far fa-trash-alt delete"></i>
+                  </li>`
+    todoList.innerHTML += html;
+};
+
+function addTodos(e) {
+    e.preventDefault();
+    const todo = submitForm.add.value.trim(); //trim extra space in input
+    if (todo.length) { // to check whether the input is empty
+        listLength = listLength + 1; // to update the checkbox id when we add a new list item
+        generateTemplate(todo);
+        submitForm.reset();
+    }
+
+}
+//add todos
+submitForm.addEventListener('submit', addTodos);
+addButton.addEventListener('click', addTodos);
